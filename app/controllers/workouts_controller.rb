1eb61eb5fd9dcfe8c8b88c641a8b9ca8b1e403c6
Olivor Hill <olivorhill@hotmail.com>
2024-10-08 15:24:27 +0100

    Added image url to workouts model, added also to seed file. Created index view page and displayed info in basic format.

--- app/controllers/workouts_controller.rb
+++ app/controllers/workouts_controller.rb
@@ -1,4 +1,5 @@
 class WorkoutsController < ApplicationController
+  skip_before_action :authenticate_user!
   def index
     @workouts = Workout.all
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_10_08_104943) do
+ActiveRecord::Schema[7.2].define(version: 2024_10_08_141026) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -57,6 +57,7 @@ ActiveRecord::Schema[7.2].define(version: 2024_10_08_104943) do
     t.string "room"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "image_url"
   end
 
   add_foreign_key "bookings", "users"
