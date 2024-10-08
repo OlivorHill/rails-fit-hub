8ac2fc8bd4c1d96ec1793fd65eb7778ecb78f9bb
Darcy Luo <darcyluojt@gmail.com>
2024-10-08 21:31:38 +0100

    try CI command from chatGPT, update homepage buttons, destroy method on bookings controller

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -2,4 +2,10 @@ class BookingsController < ApplicationController
   def create
     Booking.create(workout_id: params[:workout_id], user_id: current_user.id)
   end
+
+  def destroy
+    @Booking = Booking.find(params[:id])
+    @Booking.destroy
+    redirect_to accounts_path
+  end
 end
