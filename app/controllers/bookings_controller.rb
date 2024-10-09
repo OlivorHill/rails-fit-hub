13677fdf5d069e69557cdb8b8fec2b8ef450f2d2
Darcy Luo <darcyluojt@gmail.com>
2024-10-09 15:00:39 +0100

    put scss on show page, create seed file with user, review and bookings, show page dynamic book and booked display

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,6 +1,9 @@
 class BookingsController < ApplicationController
   def create
-    Booking.create(workout_id: params[:workout_id], user_id: current_user.id)
+    @booking = Booking.new(workout_id: params[:workout_id], user_id: current_user.id)
+    return unless @booking.save
+
+    redirect_to workout_path(params[:workout_id])
   end
 
   def destroy
